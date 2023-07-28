6d945094141524e48d9e65687f39ec7298fe7454
Daniel Díaz Ceballos <99226131+dany6x@users.noreply.github.com>
2023-07-27 22:38:00 -0600

    Update usuario.service.ts
    Actualicé service de usuario

--- tienda-en-linea1/src/app/services/usuario/usuario.service.ts
+++ tienda-en-linea1/src/app/services/usuario/usuario.service.ts
@@ -4,15 +4,22 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
+
   //url del API
   private API_SERVER = 'http://localhost:9090/guardarUsuarioDTO'
+  private API_SERVER2 = 'http://localhost:9090/listarUsuarios'
+  private API_SERVER3 = 'http://localhost:9090/borrarUsuario/'
 
-  //Constructor del HttClient
+  //Constructor del HttpClient
   constructor(private httpClient: HttpClient) { }
+
+
 
   //Método para GUARDAR un usuario
   //recibe 2 parametros que es url y el usuario
@@ -22,8 +29,18 @@ export class UsuarioService {
 
   //Método para LISTAR todos los usuarios
   public getAllUsuarios(): Observable<any> {
-    return this.httpClient.get(this.API_SERVER);
+    return this.httpClient.get(this.API_SERVER2);
   }
+
+  //Método para ELIMINAR un usuario
+  public deleteUsuario(id_usuario: any): Observable<any> {
+    return this.httpClient.delete(this.API_SERVER3 + id_usuario);
+  }
+
+  //Método para OBTENER un usuario por id
+
+
+  //Método para ACTUALIZAR un usuario
 
 
 
